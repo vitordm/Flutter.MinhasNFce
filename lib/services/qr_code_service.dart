e2f864c6198cb746316159167ad2a46354212ee3
Vitor Oliveira <vitor.oliveira@vstelecom.com.br>
2019-03-08 17:07:09 -0300

    Adicionando depencia da Lib para download de NFce

--- lib/services/qr_code_service.dart
+++ lib/services/qr_code_service.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
 
+import 'package:flutter_minhas_nfce/models/nfce.dart';
 import 'package:flutter_minhas_nfce/models/qr_code.dart';
 import 'package:flutter_minhas_nfce/services/base_service.dart';
+import 'package:flutter_minhas_nfce/services/nfce_service.dart';
+import 'package:vitor_myparser_nfce/my_parser_nfce.dart';;
 
 class QrCodeService extends BaseService {
+
+  final nfceService = NFceService();
   
   Future<QrCode> insert(QrCode qrCode) async {
     var database = await this.db.database;
@@ -16,6 +21,19 @@ class QrCodeService extends BaseService {
     var database = await this.db.database;
     var list  = await database.query('qr_code');
     return list.isNotEmpty ? list.map((qrcode) => QrCode.fromMap(qrcode)).toList() : [];
+  }
+
+  Future<NFce> syncQrCode(QrCode qrCode) async {
+    try {
+      var myParserNFceFactory = MyParserNFceFactory(qrCode.qrCode);
+      var downloadNfce = await myParserNFceFactory.make();
+      if (downloadNfce !=null) {
+        //todo: Inserir NFce
+      }
+    } catch(e) {
+      print(e);
+    }
+    return null;
   }
 
 }
